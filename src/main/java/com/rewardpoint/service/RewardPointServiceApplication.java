62292d89303ee417028ef2d18f3fe197c4ba6948
xeounxzxu <pnw123456@icloud.com>
2026-04-11 15:35:26 +0900

    build: 프로젝트 세팅

---
+++ src/main/java/com/rewardpoint/service/RewardPointServiceApplication.java
@@ -0,0 +1,12 @@
+package com.rewardpoint.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardPointServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardPointServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rewardpoint/service/RewardPointServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.rewardpoint.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardPointServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
